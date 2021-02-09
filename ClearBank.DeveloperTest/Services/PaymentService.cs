384c696ab6318ec52c0337f3509483b821759cd4
Semih Bas <smh_bas@hotmail.com>
2021-02-09 22:39:48 +0000

    fix
    fix

--- ClearBank.DeveloperTest/Services/PaymentService.cs
+++ ClearBank.DeveloperTest/Services/PaymentService.cs
@@ -37,87 +37,93 @@ namespace ClearBank.DeveloperTest.Services
         }
 
 
-       
-        public MakePaymentResult MakePaymentToRefactor(MakePaymentRequest request)
-        {
-            var dataStoreType = ConfigurationManager.AppSettings["DataStoreType"];
 
-            Account account = null;
+        #region MethodHasBeenRefactored
 
-            if (dataStoreType == "Backup")
-            {
-                var accountDataStore = new BackupAccountDataStore();
-                account = accountDataStore.GetAccount(request.DebtorAccountNumber);
-            }
-            else
-            {
-                var accountDataStore = new AccountDataStore();
-                account = accountDataStore.GetAccount(request.DebtorAccountNumber);
-            }
 
-            var result = new MakePaymentResult();
 
-            switch (request.PaymentScheme)
-            {
-                case PaymentScheme.Bacs:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
-                    {
-                        result.Success = false;
-                    }
-                    break;
+        //public MakePaymentResult MakePaymentToRefactor(MakePaymentRequest request)
+        //{
+        //    var dataStoreType = ConfigurationManager.AppSettings["DataStoreType"];
 
-                case PaymentScheme.FasterPayments:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Balance < request.Amount)
-                    {
-                        result.Success = false;
-                    }
-                    break;
+        //    Account account = null;
 
-                case PaymentScheme.Chaps:
-                    if (account == null)
-                    {
-                        result.Success = false;
-                    }
-                    else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Status != AccountStatus.Live)
-                    {
-                        result.Success = false;
-                    }
-                    break;
-            }
+        //    if (dataStoreType == "Backup")
+        //    {
+        //        var accountDataStore = new BackupAccountDataStore();
+        //        account = accountDataStore.GetAccount(request.DebtorAccountNumber);
+        //    }
+        //    else
+        //    {
+        //        var accountDataStore = new AccountDataStore();
+        //        account = accountDataStore.GetAccount(request.DebtorAccountNumber);
+        //    }
 
-            if (result.Success)
-            {
-                account.Balance -= request.Amount;
+        //    var result = new MakePaymentResult();
 
-                if (dataStoreType == "Backup")
-                {
-                    var accountDataStore = new BackupAccountDataStore();
-                    accountDataStore.UpdateAccount(account);
-                }
-                else
-                {
-                    var accountDataStore = new AccountDataStore();
-                    accountDataStore.UpdateAccount(account);
-                }
-            }
+        //    switch (request.PaymentScheme)
+        //    {
+        //        case PaymentScheme.Bacs:
+        //            if (account == null)
+        //            {
+        //                result.Success = false;
+        //            }
+        //            else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
+        //            {
+        //                result.Success = false;
+        //            }
+        //            break;
 
-            return result;
-        }
+        //        case PaymentScheme.FasterPayments:
+        //            if (account == null)
+        //            {
+        //                result.Success = false;
+        //            }
+        //            else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
+        //            {
+        //                result.Success = false;
+        //            }
+        //            else if (account.Balance < request.Amount)
+        //            {
+        //                result.Success = false;
+        //            }
+        //            break;
+
+        //        case PaymentScheme.Chaps:
+        //            if (account == null)
+        //            {
+        //                result.Success = false;
+        //            }
+        //            else if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
+        //            {
+        //                result.Success = false;
+        //            }
+        //            else if (account.Status != AccountStatus.Live)
+        //            {
+        //                result.Success = false;
+        //            }
+        //            break;
+        //    }
+
+        //    if (result.Success)
+        //    {
+        //        account.Balance -= request.Amount;
+
+        //        if (dataStoreType == "Backup")
+        //        {
+        //            var accountDataStore = new BackupAccountDataStore();
+        //            accountDataStore.UpdateAccount(account);
+        //        }
+        //        else
+        //        {
+        //            var accountDataStore = new AccountDataStore();
+        //            accountDataStore.UpdateAccount(account);
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        #endregion
     }
 }
